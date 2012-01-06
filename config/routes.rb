fce6831935d935b174ed193d7698d73f97f2f857
Matt Brictson <matt@55minutes.com>
2012-01-05 16:27:35 -0800

    Remove static index and replace with trivial view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsStarter::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ RailsStarter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
