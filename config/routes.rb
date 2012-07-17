5aed6dfe94ecffa68dac389f6dff2c1bb6afbc7c
Matt Brictson <matt@55minutes.com>
2012-07-17 16:05:02 -0700

    Rename welcome_controller to home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 RailsStarter::Application.routes.draw do
-  get "welcome/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +49,7 @@ RailsStarter::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root to: 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/acceptance/home_spec.rb
@@ -0,0 +1,10 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Home' do
+
+  scenario 'should show Home#index ' do
+    visit homepage
+    page.should have_content("Home#index")
+  end
+
+end
