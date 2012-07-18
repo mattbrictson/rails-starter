20452a72ef9beaf07587e522bafa0dbf41194c80
Matt Brictson <matt@55minutes.com>
2012-07-17 18:31:42 -0700

    Fail silently if git repo has no commit yet

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
 module RailsStarter
   VERSION = `git describe --always --tag`.chomp
-  VERSION_DATE = Date.parse(*`git log -1 --format="%ad" --date=short`)
+  VERSION_DATE = Date.parse(*`git log -1 --format="%ad" --date=short`) rescue nil
 end
