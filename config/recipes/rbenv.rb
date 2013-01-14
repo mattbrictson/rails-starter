314ac0cdb2cb58468a3e2a210a7a9d11b535500d
Matt Brictson <matt@55minutes.com>
2013-01-14 09:28:29 -0800

    Use Ubuntu boostrap provided by rbenv-installer

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -5,22 +5,10 @@ namespace :rbenv do
   task :install, roles: :app do
     run "#{sudo} aptitude -y update"
     run "#{sudo} aptitude -y install curl git-core"
-    run "#{sudo} aptitude -y install build-essential"
-    run "#{sudo} aptitude -y install zlib1g-dev libssl-dev"
-    run "#{sudo} aptitude -y install libreadline-gplv2-dev"
 
     install_or_upgrade_rbenv
-    bashrc = <<-BASHRC
-if [ -d $HOME/.rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-fi
-BASHRC
-    put bashrc, "/tmp/rbenvrc"
-    run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
-    run "mv ~/.bashrc.tmp ~/.bashrc"
-    run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
-    run %q{eval "$(rbenv init -)"}
+    modify_bashrc
+    bootstrap_ubuntu_for_ruby_compile
     compile_ruby
   end
   after "deploy:install", "rbenv:install"
@@ -28,12 +16,31 @@ BASHRC
   desc "Install the latest version of Ruby"
   task :upgrade, roles: :app do
     install_or_upgrade_rbenv
+    bootstrap_ubuntu_for_ruby_compile
     compile_ruby
   end
 end
 
 def install_or_upgrade_rbenv
   run "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
+end
+
+def modify_bashrc
+  bashrc = <<-BASHRC
+if [ -d $HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
+BASHRC
+  put bashrc, "/tmp/rbenvrc"
+  run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
+  run "mv ~/.bashrc.tmp ~/.bashrc"
+  run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
+  run %q{eval "$(rbenv init -)"}
+end
+
+def bootstrap_ubuntu_for_ruby_compile
+  run "#{sudo} $HOME/.rbenv/plugins/rbenv-installer/bin/rbenv-bootstrap-ubuntu-12-04"
 end
 
 def compile_ruby
