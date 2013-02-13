e22c5c5ec67ba9cd48485bfc4f3883a5a5003738
Matt Brictson <matt@55minutes.com>
2013-02-13 08:52:18 -0800

    Staging also uses Postmark API key from secrets; no need to override

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,16 +1,10 @@
 # Staging configuration is identical to production, with some overrides
-# for hostname, API keys, etc.
+# for hostname, etc.
 
 require_relative './production'
 
 RailsStarter::Application.configure do
 
-  # Production mailer config
-  config.to_prepare do
-    ActionMailer::Base.simple_postmark_settings = {
-      api_key: 'xxxxxxxx'
-    }
-  end
   config.action_mailer.default_url_options = {
     host: 'staging.rails-starter.com',
     protocol: 'http' # or 'https' for a secured app
