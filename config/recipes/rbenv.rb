f2783a71267060791a06efac3c3f7547da9ce609
Matt Brictson <matt@55minutes.com>
2013-04-12 14:36:14 -0700

    Add rbenv:setup and rbenv:update recipes

--- config/recipes/rbenv.rb
+++ config/recipes/rbenv.rb
@@ -6,18 +6,29 @@ namespace :rbenv do
     run "#{sudo} aptitude -y update"
     run "#{sudo} aptitude -y install curl git-core"
 
-    install_or_upgrade_rbenv
+    install_rbenv
     modify_bashrc
     bootstrap_ubuntu_for_ruby_compile
     compile_ruby
   end
   after "deploy:install", "rbenv:install"
 
+  desc "Set up standard variables for rbenv"
+  task :setup do
+    write_rbenv_var("RAILS_ENV", rails_env)
+  end
+  after "deploy:setup", "rbenv:setup"
+
   desc "Install the latest version of Ruby"
   task :upgrade do
-    install_or_upgrade_rbenv
+    update_rbenv
     bootstrap_ubuntu_for_ruby_compile
     compile_ruby
+  end
+
+  desc "Update just rbenv and its plugins, without compiling Ruby"
+  task :update do
+    update_rbenv
   end
 
   desc "Check that the specified version of Ruby is properly installed"
@@ -33,8 +44,12 @@ namespace :rbenv do
   before "deploy:update_code", "rbenv:check"
 end
 
-def install_or_upgrade_rbenv
+def install_rbenv
   run "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
+end
+
+def update_rbenv
+  run "rbenv update"
 end
 
 def modify_bashrc
@@ -46,7 +61,11 @@ fi
 BASHRC
   put bashrc, "/tmp/rbenvrc"
   run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
-  run "mv ~/.bashrc.tmp ~/.bashrc"
+
+  # Only replace ~/.bashrc if it doesn't already contain "rbenv init"
+  run "grep -qs 'rbenv init' ~/.bashrc || mv ~/.bashrc.tmp ~/.bashrc"
+
+  # Load rbenv into the current shell
   run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
   run %q{eval "$(rbenv init -)"}
 end
@@ -60,4 +79,15 @@ def compile_ruby
   run "rbenv global #{ruby_version}"
   run "gem install bundler --no-ri --no-rdoc"
   run "rbenv rehash"
+end
+
+def write_rbenv_var(name, value)
+  run "touch ~/.rbenv/vars"
+  run "chmod 0600 ~/.rbenv/vars"
+
+  run "sed --in-place '/^#{name}=/d' ~/.rbenv/vars"
+
+  put "#{name}=#{value}\n", "/tmp/rbenv_var"
+  run "cat /tmp/rbenv_var >> ~/.rbenv/vars"
+  run "rm /tmp/rbenv_var"
 end
