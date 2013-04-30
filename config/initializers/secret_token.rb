337f977257393822326802c07ad0da42dfd4af16
Matt Brictson <matt@55minutes.com>
2013-04-30 15:55:03 -0700

    rails new rails-starter -d postgresql -T (4.0.0.rc1)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsStarter::Application.config.secret_token = '69560e72e3063ee6399dd6976b4a0fed3ffdb7c3065adbd911e5868c8600953c4743cbd93cf95d33b480824af966aa2ad847eec9040b6be98062e25edff3f442'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsStarter::Application.config.secret_key_base = '39a0724c4fa9bbce18b7599e23c3f655962729fe9f0893c087db7c0396e07bc551095406ec450965a664d721fa415423b76145c136fc5a9ccb9f9c6624984951'
