0e8e59ebeb76efeb31aa735aeaa199d9420b65d3
Matt Brictson <matt@55minutes.com>
2013-05-18 12:08:29 -0700

    eager_load needs to explicitly set in Rails 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,15 @@
 RailsStarter::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Show full error reports and disable caching
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
