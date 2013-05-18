4d327b94def7a3817729cb0eb20eee591f400dd3
Matt Brictson <matt@55minutes.com>
2013-05-18 12:58:27 -0700

    More minor tweaks for Rails 3.2 -> 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
   ensure_security_headers
 end
