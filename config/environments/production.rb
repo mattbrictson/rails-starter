5577e587035745533292cfdc7db4af65a2a56a30
Matt Brictson <matt@55minutes.com>
2013-05-21 11:54:24 -0700

    Use SECRETS.fetch() to avoid nils silently being used

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ RailsStarter::Application.configure do
   config.action_mailer.delivery_method = :simple_postmark
   config.to_prepare do
     ActionMailer::Base.simple_postmark_settings = {
-      api_key: SECRETS[:postmark_api_key]
+      api_key: SECRETS.fetch(:postmark_api_key)
     }
   end
   config.action_mailer.raise_delivery_errors = true
