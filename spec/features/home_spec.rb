67f6258cb06c4790275d1614ba1989d1c0fc69f5
Matt Brictson <matt@55minutes.com>
2013-07-16 10:50:30 -0700

    Add useful assertion to check there are no JS errors

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 feature 'Home' do
 
-  scenario 'should show Home#index' do
-    visit homepage
+  it 'should show Home#index' do
+    visit(homepage)
     expect(page).to have_content("Home#index")
+  end
+
+  it 'should not have JavaScript errors', :js => true do
+    visit(homepage)
+    expect(page).not_to have_errors
   end
 
 end
