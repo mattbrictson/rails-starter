b0335e81a765f2400476870bc244ec2c9cd8e685
Matt Brictson <matt@55minutes.com>
2013-08-22 16:59:59 -0700

    Move recipes into new capistrano-fiftyfive gem

--- config/recipes/seeds.rb
+++
@@ -1,7 +0,0 @@
-namespace :seeds do
-  desc "Run rake db:seed"
-  task :execute, :roles => :db, :only => { :primary => true } do
-    run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} db:seed"
-  end
-  after "deploy:migrate", "seeds:execute"
-end
