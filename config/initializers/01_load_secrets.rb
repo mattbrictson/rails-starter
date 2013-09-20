bb04a6c4fba2915e09d5ebb79a75cd942c472a6d
Matt Brictson <matt@55minutes.com>
2013-09-19 19:52:48 -0700

    Allow secrets to be overridden by env vars.
    This allows the secrets system to be used in places like Heroku,
    where environment variables are preferred over configuration files.

--- config/initializers/01_load_secrets.rb
+++ config/initializers/01_load_secrets.rb
@@ -1,2 +1,9 @@
-raw_config = File.read("#{Rails.root}/config/secrets.yml")
-SECRETS = YAML.load(raw_config)[Rails.env].symbolize_keys
+yml_file = "#{Rails.root}/config/secrets.yml"
+
+yml_secrets = if File.exist?(yml_file)
+  YAML.load_file(yml_file)[Rails.env].with_indifferent_access
+else
+  {}
+end
+
+SECRETS = yml_secrets.merge(ENV)
