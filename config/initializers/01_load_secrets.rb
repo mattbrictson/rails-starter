d15dc7ea423e6ff39dec5b1e5801ea3e0af4e968
Matt Brictson <matt@55minutes.com>
2013-09-21 15:15:13 -0700

    Use with_indifferent_access even in no-YAML case

--- config/initializers/01_load_secrets.rb
+++ config/initializers/01_load_secrets.rb
@@ -1,9 +1,9 @@
 yml_file = "#{Rails.root}/config/secrets.yml"
 
 yml_secrets = if File.exist?(yml_file)
-  YAML.load_file(yml_file)[Rails.env].with_indifferent_access
+  YAML.load_file(yml_file)[Rails.env]
 else
   {}
 end
 
-SECRETS = yml_secrets.merge(ENV)
+SECRETS = yml_secrets.with_indifferent_access.merge(ENV)
