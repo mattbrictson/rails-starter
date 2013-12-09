2c196fd691d65473a1cbb72ab760c39629079259
Matt Brictson <matt@55minutes.com>
2013-12-08 20:42:29 -0800

    Move I18n fix earlier in the boot process

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+I18n.enforce_available_locales = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -24,6 +26,5 @@ module RailsStarter
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.enforce_available_locales = true
   end
 end
