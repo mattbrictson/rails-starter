ed58a19e539ab756ef86fdf79096d63b0a8d9542
Matt Brictson <matt@55minutes.com>
2014-08-29 06:46:44 -0700

    Enable color output for capistrano via jenkins

--- jenkins-ci.sh
+++ jenkins-ci.sh
@@ -50,6 +50,6 @@ if bundle show capistrano &> /dev/null; then
   if [[ $GIT_BRANCH == origin/development ]]; then
     eval `ssh-agent -s`
     ssh-add
-    bundle exec cap staging deploy:migrate_and_restart
+    SSHKIT_COLOR=1 bundle exec cap staging deploy:migrate_and_restart
   fi
 fi
